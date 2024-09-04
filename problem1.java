82699bb6a319d83c031bff7145f3cd7e4104ecc0
angelaphan <aphan117@student.hbuhsd.edu>
2024-09-04 10:06:10 -0700

    b

--- problem1.java
+++ problem1.java
@@ -1,1 +1,9 @@
 // Create your class here; don't forget the main method
+public class problem1
+{
+    public static void main(String[] args)
+    {
+        String food = "pasta";
+        System.out.print("I like to eat "+ food);
+    }
+}
